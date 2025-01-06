9663bdc6e31e2825c87a06e93b54753bbc4701d1
Raí Araujo <raisanhacu@gmail.com>
2025-01-06 15:21:44 -0300

    🛠️ chore(api): Configurações de integração com a API do google maps

---
+++ src/api/maps/maps.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+
+@Injectable()
+export class MapsService {
+  private readonly googleApiKey = process.env.GOOGLE_MAPS_API_KEY;
+  private readonly opencageApiKey = process.env.OPENCAGE_API_KEY;
+  private readonly googleBaseUrl = 'https://maps.googleapis.com/maps/api';
+  private readonly opencageBaseUrl = 'https://api.opencagedata.com/geocode/v1';
+
+  constructor(private readonly httpService: HttpService) {}
+
+  async calculateDistance(origin: string, destination: string): Promise<any> {
+    // use the google maps distance Matrix to calculate the distance between two locations
+    const url = `${this.googleBaseUrl}/distancematrix/json?origins=${origin}&destinations=${destination}&key=${this.googleApiKey}`;
+
+    const googleResponse = await this.httpService.get(url).toPromise();
+
+    // fallback for specific cases where the google response is not as expected, Use OpenCageData API as a backup
+    if (
+      !googleResponse.data.origin_addresses.length ||
+      !googleResponse.data.destination_addresses.length ||
+      googleResponse.data.rows[0]?.elements[0]?.status !== 'OK'
+    ) {
+      const fallbackCoordinates = await this.getFallbackCoordinates(origin, destination);
+      const url = `${this.googleBaseUrl}/distancematrix/json?origins=${fallbackCoordinates.origin.lat},${fallbackCoordinates.origin.lng}&destinations=${fallbackCoordinates.destination.lat},${fallbackCoordinates.destination.lng}&key=${this.googleApiKey}`;
+      const googleResponse = await this.httpService.get(url).toPromise();
+      return googleResponse.data;
+    }
+
+    return googleResponse.data;
+  }
+
+  // get the coordinates from the origin and destination using OpenCageData API
+  private async getFallbackCoordinates(origin: string, destination: string): Promise<any> {
+    try {
+      // use promise.all to make both requests at the same time
+      const [originGeocode, destinationGeocode] = await Promise.all([
+        this.getCoordinatesFromOpenCage(origin),
+        this.getCoordinatesFromOpenCage(destination),
+      ]);
+  
+      if (!originGeocode || !destinationGeocode) {
+        throw new Error('Unable to retrieve coordinates from OpenCageData');
+      }
+  
+      return {
+        origin: originGeocode,
+        destination: destinationGeocode,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+
+  private async getCoordinatesFromOpenCage(address: string): Promise<any> {
+    const url = `${this.opencageBaseUrl}/json?q=${encodeURIComponent(address)}&key=${this.opencageApiKey}`;
+    const response = await this.httpService.get(url).toPromise();
+
+    const data = response.data?.results[0];
+    if (!data) {
+      console.log('No results from OpenCageData');
+      return null;
+    }
+
+    return {
+      lat: data.geometry.lat,
+      lng: data.geometry.lng,
+      formatted: data.formatted,
+    };
+  }
+}
