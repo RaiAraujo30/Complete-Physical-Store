3e2388ae5df34ac9bdcfbb3bd5744ab1fd1b0081
Raí Araujo <raisanhacu@gmail.com>
2025-01-06 15:55:33 -0300

    ♻️  refactor(store): Refatoração da função getStoresWithShipping

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -63,124 +63,124 @@ export class StoreService {
       throw new NotFoundException(`Store with ID ${id} not found`);
     }
   }
-  async calculateDistancesFromCep(
-    cep: string,
-  ): Promise<{ store: Store; distance: number }[]> {
+  async calculateDistancesFromCep(cep: string): Promise<{ store: Store; distance: number }[]> {
     const stores = await this.listAll();
 
     const distances = await Promise.all(
       stores.map(async (store) => {
-        const origin = cep;
-        const destination = `${store.latitude},${store.longitude}`;
-        const response = await this.mapsService.calculateDistance(
-          origin,
-          destination,
-        );
-
-        const distanceInMeters = response.rows[0]?.elements[0]?.distance?.value;
-        if (distanceInMeters !== undefined) {
-          const distanceInKm = parseFloat((distanceInMeters / 1000).toFixed(1));
-          return { store, distance: distanceInKm };
+        const distance = await this.calculateDistances(cep, `${store.latitude},${store.longitude}`);
+        if (distance !== null) {
+          return { store, distance };
         }
-        return null; // Caso não consiga calcular a distância, retorna null
+        return null;
       }),
     );
 
-    // Filtrar nulos e ordenar as lojas pela distância
     return distances.filter((item) => item !== null).sort((a, b) => a.distance - b.distance);
   }
 
-  // Retornar lojas com informações de frete
+  private async calculateDistances(origin: string, destination: string): Promise<number | null> {
+    try {
+      const response = await this.mapsService.calculateDistance(origin, destination);
+      const distanceInMeters = response.rows[0]?.elements[0]?.distance?.value;
+      return distanceInMeters !== undefined ? parseFloat((distanceInMeters / 1000).toFixed(1)) : null;
+    } catch (error) {
+      console.error(`Erro ao calcular distância:`, error.message);
+      return null;
+    }
+  }
+
   async getStoresWithShipping(cep: string): Promise<{ stores: any[]; pins: any[] }> {
     const distances = await this.calculateDistancesFromCep(cep);
     const stores = [];
     const pins = [];
 
     for (const { store, distance } of distances) {
-      // Criar o objeto do pin
-      pins.push({
-        position: {
-          lat: parseFloat(store.latitude),
-          lng: parseFloat(store.longitude),
-        },
-        title: store.storeName,
-      });
+      pins.push(this.createPin(store));
 
       if (store.type === 'PDV' && distance <= 50) {
-        // Adiciona o PDV com frete fixo
-        stores.push({
-          name: store.storeName,
-          city: store.city,
-          postalCode: store.postalCode,
-          type: store.type,
-          distance:` ${distance} km`,
-          value: [
-            {
-              prazo: '1 dias úteis',
-              price: 'R$ 15,00',
-              description: 'Motoboy',
-            },
-          ],
-        });
+        stores.push(this.createPdvStoreWithFixedShipping(store, distance));
       } else {
-        // Adiciona lojas ou PDVs fora do raio com cálculo de frete
-        try {
-          const freightValues = await this.correiosService.calculateFreight({
-            cepDestino: cleanCep(String(cep)),
-            cepOrigem: cleanCep(String(store.postalCode)),
-            comprimento: '30',
-            largura: '15',
-            altura: '10',
-          });
-
-          stores.push({
-            name: store.storeName,
-            city: store.city,
-            postalCode: store.postalCode,
-            type: store.type,
-            distance: ` ${distance} km`,
-            value: freightValues.map((freight: any) => ({
-              price: freight.precoAgencia,
-              prazo: freight.prazo,
-              description: freight.urlTitulo,
-            })),
-          });
-        } catch (error) {
-          console.error(
-            `Erro ao calcular frete para a loja ${store.storeName}:,
-            error.message`);
-          stores.push({
-            name: store.storeName,
-            city: store.city,
-            postalCode: store.postalCode,
-            type: store.type,
-            distance: ` ${distance} km`,
-            value: [
-              {
-                price: 'Indisponível',
-                prazo: 'Indisponível',
-                description: 'Erro ao calcular o frete',
-              },
-            ],
-          });
-        }
+        stores.push(await this.createStoreWithDynamicShipping(store, cep, distance));
       }
     }
 
     return { stores, pins };
   }
 
-  async filterStoresWithinRadius(
-    cep: string,
-    radiusInKm: number = 50,
-  ): Promise<{ store: Store; distance: number }[]> {
-    const distances = await this.calculateDistancesFromCep(cep);
-
-    return distances.filter(({ distance }) => distance <= radiusInKm);
+  private createPin(store: Store): any {
+    return {
+      position: {
+        lat: parseFloat(store.latitude),
+        lng: parseFloat(store.longitude),
+      },
+      title: store.storeName,
+    };
   }
 
-}
+  private createPdvStoreWithFixedShipping(store: Store, distance: number): any {
+    return {
+      name: store.storeName,
+      city: store.city,
+      postalCode: store.postalCode,
+      type: store.type,
+      distance: `${distance} km`,
+      value: [
+        {
+          prazo: '1 dias úteis',
+          price: 'R$ 15,00',
+          description: 'Motoboy',
+        },
+      ],
+    };
+  }
 
-function cleanCep(cep: string): string {
-  return cep.replace(/\D/g, ''); // Remove qualquer caractere não numérico
+  private async createStoreWithDynamicShipping(
+    store: Store,
+    cep: string,
+    distance: number,
+  ): Promise<any> {
+    try {
+      const freightValues = await this.correiosService.calculateFreight({
+        cepDestino: this.cleanCep(cep),
+        cepOrigem: this.cleanCep(store.postalCode),
+        comprimento: '30',
+        largura: '15',
+        altura: '10',
+      });
+
+      return {
+        name: store.storeName,
+        city: store.city,
+        postalCode: store.postalCode,
+        type: store.type,
+        distance: `${distance} km`,
+        value: freightValues.map((freight: any) => ({
+          price: freight.precoAgencia,
+          prazo: freight.prazo,
+          description: freight.urlTitulo,
+        })),
+      };
+    } catch (error) {
+      console.error(`Erro ao calcular frete para a loja ${store.storeName}:`, error.message);
+      return {
+        name: store.storeName,
+        city: store.city,
+        postalCode: store.postalCode,
+        type: store.type,
+        distance: `${distance} km`,
+        value: [
+          {
+            price: 'Indisponível',
+            prazo: 'Indisponível',
+            description: 'Erro ao calcular o frete',
+          },
+        ],
+      };
+    }
+  }
+
+  private cleanCep(cep: string): string {
+    return cep.replace(/\D/g, '');
+  }
 }
