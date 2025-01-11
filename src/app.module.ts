4fe55dc14b3f26bf7ed1b656ffdaf0dea93c1bde
Raí Araujo <raisanhacu@gmail.com>
2025-01-11 14:37:59 -0300

    ♻️  refactor: Separação da config do banco de dados do modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { StoreModule } from './store/store.module';
 import { DeliveryCriteriaModule } from './delivery/delivery-criteria.module';
+import { DatabaseModule } from './config/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_CONNECT),
+    DatabaseModule,
     StoreModule,
     DeliveryCriteriaModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+
 })
 export class AppModule {}
